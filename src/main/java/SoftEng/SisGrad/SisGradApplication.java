cd2c83f550bf6c4f81143587b3d8f937359f1763
Guilherme Barbosa <coalamct@gmail.com>
2021-06-24 14:07:12 -0300

    Start

---
+++ src/main/java/SoftEng/SisGrad/SisGradApplication.java
@@ -0,0 +1,13 @@
+package SoftEng.SisGrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisGradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisGradApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SoftEng/SisGrad/SisGradApplicationTests.java
@@ -0,0 +1,13 @@
+package SoftEng.SisGrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisGradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
