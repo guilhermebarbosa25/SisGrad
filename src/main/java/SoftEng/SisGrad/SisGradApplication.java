7757388c5a5d4ff16bf44fca5acebc1f072d1c03
guilhermerbarbosa25 <guilhermebarbosa25@github.com>
2021-08-18 15:57:06 -0300

    Post Final

--- src/main/java/SoftEng/SisGrad/SisGradApplication.java
+++ src/main/java/SoftEng/SisGrad/SisGradApplication.java
@@ -1,6 +1,7 @@
 package SoftEng.SisGrad;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +11,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({"Models","Controllers","Models.Repositories"})
 @EnableJpaRepositories("Models")
 @EntityScan({"Models"})
+@EnableAutoConfiguration
 
 public class SisGradApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SisGradApplication.class, args);
 	}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+create schema if not exists dbSisGrad;
+
+create table IF NOT EXISTS dbSisGrad.usuario(matricula varchar(255),cpf varchar(255),datanasc varchar(255),email varchar(255),nome varchar(255),senha varchar(255),usertype varchar(255));
+
+insert into dbSisGrad.usuario(matricula, cpf, datanasc, email, nome, senha, usertype)
+Select 'U00000','000.000.000-00', '01/01/2000', 'adm@sisgrad.com', 'admin', 'admin', 'Administrativo'
+where not exists (select * from dbSisGrad.usuario where email='adm@sisgrad.com');
